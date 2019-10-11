b295333e59c9abf25d581163d295a700b7d22568
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-11 10:59:18 -0700

    added notes

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -95,16 +97,42 @@ class SortingRobot:
     def sort(self):
         """
         Sort the robot's list.
+
+        self.swap_item
+        swap item swaps the currently held item with self.postion
+
+        self.compare_item
+        compare item compares the currently held item with the item in self.positon
+
+        self.can_move_right
+        checks if can move further right
+
+        self.can_move_left
+        check if can move left
+
+        self.move_left
+        moves position -1
+
+        self.move_right
+        moves positon + 1
+
+        so I can implement a bubble sort by putting none in the first column
+        and then comparing item with 2, so I would have to move right, compare,
+        move left and them swap items, and keep repeating.
+        swap item
+        move right
+        compare item if item held is greater swap item
+        move left
+        swap item
+        and repeat
         """
-        # Fill this out
-        pass
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [3, 2, 4, 5, 1]
 
     robot = SortingRobot(l)
 
